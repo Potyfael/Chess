3ee96d2a89d91909299280e72afe0d35a2a0deab
Potyfael <potyfael@gmail.com>
2024-06-25 15:08:48 -0300

    First class: Position

---
+++ chess/src/boardgame/Position.java
@@ -0,0 +1,38 @@
+package boardgame;
+
+public class Position {
+
+    private Integer row;
+    private Integer col;
+
+    public Position(Integer row, Integer col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getCol() {
+        return col;
+    }
+
+    public void setCol(Integer col) {
+        this.col = col;
+    }
+
+    public void setValues(int row, int col) {
+        setRow(row);
+        setCol(col);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d, %d", row, col);
+    }
+}
